48461804fc3d42d96151f08ca9659a8a9712a1d9
keep70326 <keep70326@gmail.com>
2022-07-27 16:49:06 +0800

    first commit

---
+++ new_contract.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.24;
+
+contract Factory {
+    address[] public newContracts;
+
+    function createContract(string name) public {
+        address newContract = new Contract(name);
+        newContracts.push(newContract);
+    } 
+}
+
+contract Contract {
+    string public Name;
+    constructor (string _name) public {
+        Name = _name;
+    }
+    function setName(string _name) public {
+        Name = _name;
+    }
+}
+
+// contract interaction {
+//     Contract contract_ = Contract(0x123); 
+//     function setSomething(string _name) public {
+//         contract_.setName(_name);
+//     }
+// }
