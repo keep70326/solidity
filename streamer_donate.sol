edcda4117ec787fbce84ca136f456c93c723e1d6
keep70326 <keep70326@gmail.com>
2022-08-04 19:39:49 +0800

    to be continued

--- streamer_donate.sol
+++ streamer_donate.sol
@@ -11,17 +11,22 @@ contract donate{
     event logDonate(address streamer, address donor, string nickname, uint value, string message);
 
     function donate(address _streamer,string _nickname, string _message) public payable {
-        _streamer.transfer(msg.value);
+        require(msg.value > 0);
+        streamer.transfer(msg.value);
         
-        donationHistory[_streamer].ledger[msg.sender] += msg.value;
-        donationHistory[_streamer].donors.push(msg.sender);
-
+        if (donationHistory[_streamer].ledger[msg.sender] == 0){
+            donationHistory[_streamer].donors.push(msg.sender);
+        }
+        donationHistory[_streamer].ledger[msg.sender] += msg.value; //ledger[msg.sender是抖內多少錢
+        
         emit logDonate(_streamer, msg.sender, _nickname, msg.value, _message);
     }
 
     function getDonateList() public view returns(address[]){
-
+        return (donationHistory[_streamer].donors);
     }
+
+    event logListDonorInfo()
 
     function listDonorInfo() public {
 
