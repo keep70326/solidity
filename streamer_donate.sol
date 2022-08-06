447b2ccab6a2893b44ad11da23c2b208890ef191
keep70326 <keep70326@gmail.com>
2022-08-06 17:18:12 +0800

    finished this project

--- streamer_donate.sol
+++ streamer_donate.sol
@@ -1,34 +1,39 @@
 pragma solidity ^0.4.24;
 
-contract donate{
+contract Donate{
 
     struct donorInfo{
         address[] donors;
-        mapping (address => uint) ledger //乾爹 ＝> 已經抖內多少錢
+        mapping (address => uint) ledger; //該名乾爹＝>已抖內該實況主多少
     }
-    mapping (address => donorInfo) donationHistory;// 實況主地址＝> 抖內訊息
+    mapping (address => donorInfo) donationHistory;
+
 
     event logDonate(address streamer, address donor, string nickname, uint value, string message);
 
-    function donate(address _streamer,string _nickname, string _message) public payable {
-        require(msg.value > 0);
-        streamer.transfer(msg.value);
-        
+    function donate(address _streamer, string _nickname, string _message) public payable {
+        require(msg.value > 0, '別想白嫖，要互動請抖內') ;
+        _streamer.transfer(msg.value);
+
         if (donationHistory[_streamer].ledger[msg.sender] == 0){
             donationHistory[_streamer].donors.push(msg.sender);
-        }
-        donationHistory[_streamer].ledger[msg.sender] += msg.value; //ledger[msg.sender是抖內多少錢
-        
+        } 
+        donationHistory[_streamer].ledger[msg.sender] += msg.value;
+
         emit logDonate(_streamer, msg.sender, _nickname, msg.value, _message);
     }
-
-    function getDonateList() public view returns(address[]){
-        return (donationHistory[_streamer].donors);
+    
+    //實況主呼叫方法查詢乾爹名單
+    function getDonorList() public view returns(address[]){ 
+        return (donationHistory[msg.sender].donors);
     }
 
-    event logListDonorInfo()
-
+    //實況主呼叫方法查詢乾爹明細
+    event logListDonorInfo(address streamer, address donors, uint value);
     function listDonorInfo() public {
-
+        for (uint i=0; i < donationHistory[msg.sender].donors.length; i++){
+            address daddy = donationHistory[msg.sender].donors[i];
+            emit logListDonorInfo(msg.sender, daddy, donationHistory[msg.sender].ledger[daddy]);
+        }
     }
 }
