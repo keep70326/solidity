5381e642e25b0b0e706926973ee36dfda489b7d3
keep70326 <keep70326@gmail.com>
2022-08-10 10:17:06 +0800

    finished

--- ERC20/erc20_Pausable.sol
+++ ERC20/erc20_Pausable.sol
@@ -1,18 +1,22 @@
 pragma solidity ^0.4.24;
 
 contract pausable{
-    bool private _pausable;
+    bool private _paused;
+    address private owner;
 
     constructor() public {
-        _pausable = false;
+        _paused = false; //初始合約非暫停,要暫停變成true
+        owner = msg.sender;
     }
 
     modifier whenPaused(){
+        require(msg.sender == owner);
         require(_paused);
         _;
     }
 
     modifier whenNotPaused(){
+        require(msg.sender == owner);
         require(!_paused);
         _;
     }
@@ -21,13 +25,13 @@ contract pausable{
     event Unpause(address addr);
 
     function pasue() public whenPaused returns(bool){
-        _pausable = false;
+        _paused = false;
         emit Unpause(msg.sender);
         return true;
     }
 
     function unpause() public whenNotPaused returns(bool){
-        _pausable = true;
+        _paused = true;
         emit Pause(msg.sender);
         return true;
     }
