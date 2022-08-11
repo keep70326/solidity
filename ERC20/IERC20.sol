cf382a5b4369f1235796baf85aea5b15c2812ec5
keep70326 <keep70326@gmail.com>
2022-08-11 15:37:13 +0800

    revised

--- ERC20/IERC20.sol
+++ ERC20/IERC20.sol
@@ -13,16 +13,13 @@ interface IERC20 {
   
   
   // 得到 tokenOwner 授權給 spender 使用的 Token 剩餘數量
-  function allowance(address tokenOwner, address spender)
-    external view returns (uint256 remaining);
+  function allowance(address tokenOwner, address spender) external view returns (uint256 remaining);
   
   // msg.sender 授權給 spender 可使用自己的 tokens 個 Token
-  function approve(address spender, uint256 tokens)
-    external returns (bool success);
+  function approve(address spender, uint256 tokens) external returns (bool success);
 
   // 將 tokens 個 Token 從 from 轉到 to
-  function transferFrom(address from, address to, uint256 tokens)
-    external returns (bool success);
+  function transferFrom(address from, address to, uint256 tokens) external returns (bool success);
 
   event Transfer(
     address indexed from,
