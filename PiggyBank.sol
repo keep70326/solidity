6aa7935f044809edfc0c74b1a0eabfb94a61811d
keep70326 <keep70326@gmail.com>
2022-08-15 12:04:25 +0800

    finsihed

---
+++ PiggyBank.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.1.0;
+
+contract PiggyBank {
+    uint public goal;
+
+   constructor (uint _goal) {
+        goal = _goal;
+    }
+
+    receive () external payable{
+        
+    }
+
+    function getMyBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function withdraw() public{
+        if(getMyBalance() > goal){
+           selfdestruct(msg.sender); 
+        }
+    }
+}
